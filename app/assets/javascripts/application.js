ee3b23747bceeb54aeb062e39f149ea0b745a369
missmaggiemo <margaret.m.moreno@gmail.com>
2014-03-20 15:33:07 -0700

    require tree no longer directs to templates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require underscore
 //= require backbone
 //= require todo_app
-//= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
