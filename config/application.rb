573b24096ead07253e17e0995fb634f427d0a6c5
missmaggiemo <margaret.m.moreno@gmail.com>
2014-03-20 15:30:28 -0700

    static assets settings

--- config/application.rb
+++ config/application.rb
@@ -2,18 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Pick the frameworks you want:
-# require "active_record/railtie"
-# require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
-config.serve_static_assets = true
 
 module TodoApp
   class Application < Rails::Application
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ TodoApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.serve_static_assets = true
+
 end
